98c8e42dca27ec7ec8504d6116af8b4b6d61bf21
Kuznetsov F <71835940+pyfuhr@users.noreply.github.com>
2022-04-12 10:37:59 +0300

    Update re_microcrypt.py

--- re_microcrypt.py
+++ re_microcrypt.py
@@ -1,11 +1,14 @@
 from num_sublib import gcd, modinv, generatePrime
 
 class RSA():
-    def __init__(self, len):
-        self.p = generatePrime(len)
-        self.q = generatePrime(len)
-        while self.q == self.p:
-            self.q =generatePrime(len)
+    def __init__(self, **kwargs):
+        ln = kwargs.get('ln')
+        self.p, self.q = kwargs.get('p'), kwargs.get('q')
+        if not ln is None:
+            self.p = generatePrime(ln)
+            self.q = generatePrime(ln)
+            while self.q == self.p:
+                self.q = generatePrime(ln)
         self.n = self.p*self.q
         self.f = (self.p-1)*(self.q-1)
         self.e = 65537
@@ -13,17 +16,43 @@ class RSA():
         self.pubk = PubK(self.e, self.n)
         self.privk = PrivK(self.d, self.n)
 
+def del0(s):
+    k = 0
+    for i in range(len(s)):
+        if s[i] == 0:
+            k += 1
+        else:
+            break
+    return s[k:]
 
-class PubK():
+class RSAPubK:
     def __init__(self, e, n):
         self.e = e
         self.n = n
-    def encrypt(self, text):
-        return pow(text, self.e, self.n)
 
-class PrivK():
+    def encrypt(self, text):
+        text = int.from_bytes(text, 'big')
+        return del0(int.to_bytes(pow(text, self.e, self.n), 1024, 'big'))
+    
+    def check(self, text, data, hashfunc):
+        if self.encrypt(text) == hashfunc(data): return True
+        else: False
+
+    def __str__(self):
+        return str({'e': self.e, 'n': self.n})
+
+
+class RSAPrivK:
     def __init__(self, d, n):
         self.d = d
         self.n = n
+
     def decrypt(self, etext):
-        return pow(etext, self.d, self.n)
+        etext = int.from_bytes(etext, 'big')
+        return del0(int.to_bytes(pow(etext, self.d, self.n), 1024, 'big'))
+    
+    def sign(self, data):
+        return self.decrypt(data)
+
+    def __str__(self):
+        return str({'d': self.d, 'n': self.n})
