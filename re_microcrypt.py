1d11afd7ff9b01193869f33fd610768669b1b311
Kuznetsov F <71835940+pyfuhr@users.noreply.github.com>
2022-04-12 11:03:23 +0300

    Update re_microcrypt.py

--- re_microcrypt.py
+++ re_microcrypt.py
@@ -34,9 +34,14 @@ class RSAPubK:
         text = int.from_bytes(text, 'big')
         return del0(int.to_bytes(pow(text, self.e, self.n), 1024, 'big'))
     
-    def check(self, text, data, hashfunc):
-        if self.encrypt(text) == hashfunc(data): return True
-        else: False
+    def check(self, text, data=None, hashfunc=None, hashedData=None):
+        if not (data is None) and not (hashfunc is None):
+            if self.encrypt(text) == hashfunc(data): return True
+            else: return False
+        if not (hashedData is None):
+            if self.encrypt(text) == hashedData: return True
+            else: return False
+        raise ValueError('Not enought args: text + (data+hashfunc or hashedData)')
 
     def __str__(self):
         return str({'e': self.e, 'n': self.n})
@@ -51,8 +56,12 @@ class RSAPrivK:
         etext = int.from_bytes(etext, 'big')
         return del0(int.to_bytes(pow(etext, self.d, self.n), 1024, 'big'))
     
-    def sign(self, data):
-        return self.decrypt(data)
+    def sign(self, data=None, hashfunc=None, hashedData=None):
+        if not (data is None) and not (hashfunc is None):
+            return self.decrypt(hashfunc(data))
+        if not (hashedData is None):
+            return self.decrypt(hashedData)
+        raise ValueError('Not enought args: data+hashfunc or hashedData')
 
     def __str__(self):
         return str({'d': self.d, 'n': self.n})
