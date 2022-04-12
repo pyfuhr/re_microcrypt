baf887d5fc8ef9fad180771a03145485e9bb7102
Kuznetsov F <71835940+pyfuhr@users.noreply.github.com>
2022-04-12 10:46:26 +0300

    Update num_sublib.py

--- num_sublib.py
+++ num_sublib.py
@@ -5,7 +5,7 @@ def extgcd(a, b):
     if a == 0:
         return (b, 0, 1)
     else:
-        g, y, x = egcd(b % a, a)
+        g, y, x = extgcd(b % a, a)
         return (g, x - (b // a) * y, y)
 
 def modinv(a, m):
